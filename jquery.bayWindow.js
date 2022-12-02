1aecee4d1f7e3886b9f07fa56cdc73f48407cf8a
zhaowenzheng <184418734@qq.com>
2022-12-02 15:05:36 +0800

    add target and closeText

--- jquery.bayWindow.js
+++ jquery.bayWindow.js
@@ -1,61 +1,42 @@
 /*!
  * jquery.bayWindow 0.0.1 (https://github.com/trague/jquery.bayWindow)
- * Author: zhao wenZheng
- *
- * Licensed under MIT (https://github.com/craftpip/jquery-confirm/blob/master/LICENSE)
+ * Author: Zhao WenZheng
+ * Contact: 455147862@qq.com
+ * Copyright 2022 jquery.bayWindow
+ * Licensed under MIT (https://github.com/trague/jquery.bayWindow/blob/main/LICENSE)
  */
 
-// requestAnimationFrame polyfill
-(function () {
-  "use strict";
-
-  // ie <= 8
-  if (!Date.now)
-    Date.now = function () {
-      return new Date().getTime();
-    };
-
-  // ie <= 9
-  var vendors = ["webkit", "moz"];
-  for (var i = 0; i < vendors.length && !window.requestAnimationFrame; ++i) {
-    var vp = vendors[i];
-    window.requestAnimationFrame = window[vp + "RequestAnimationFrame"];
-    window.cancelAnimationFrame =
-      window[vp + "CancelAnimationFrame"] ||
-      window[vp + "CancelRequestAnimationFrame"];
-  }
-  if (
-    /iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent) || // iOS6 is buggy
-    !window.requestAnimationFrame ||
-    !window.cancelAnimationFrame
-  ) {
-    var lastTime = 0;
-    window.requestAnimationFrame = function (callback) {
-      var now = Date.now();
-      var nextTime = Math.max(lastTime + 16, now);
-      return setTimeout(function () {
-        callback((lastTime = nextTime));
-      }, nextTime - now);
-    };
-    window.cancelAnimationFrame = clearTimeout;
-  }
-})();
-
-(function (factory) {
-  if (typeof define === "function" && define.amd) {
-    // AMD
-    define(["jquery"], factory);
-  } else if (typeof exports === "object") {
-    // CommonJS
-    factory(require("jquery"));
+;(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = function (root, jQuery) {
+      if (jQuery === undefined) {
+        // require('jQuery') returns a factory that requires window to
+        // build a jQuery instance, we normalize how we use modules
+        // that require this pattern but the window provided is a noop
+        // if it's defined (how jquery works)
+        if (typeof window !== 'undefined') {
+          jQuery = require('jquery')
+        } else {
+          jQuery = require('jquery')(root)
+        }
+      }
+      factory(jQuery)
+      return jQuery
+    }
   } else {
     // Browser globals
-    factory(jQuery);
+    factory(jQuery)
   }
 })(function ($) {
-  var BAY_WINDOW_DATA_KEY = "bayWindow";
+  requestAnimationFramePolyfill()
+  // data key
+  var BAY_WINDOW_DATA_KEY = 'bayWindow'
   // ie <= 9, 是否兼容transform
-  var SUPPORT_TRANSFORM = "transform" in document.documentElement.style;
+  var SUPPORT_TRANSFORM = 'transform' in document.documentElement.style
 
   $.extend({
     /**
@@ -64,37 +45,60 @@
      * @returns $bayWindow
      */
     createBayWindowElement: function (options) {
+      var appendStyleTag = function () {
+        var tagId = 'createBayWindowElementStyleTag'
+        if ($('#' + tagId).length) return
+        // prettier-ignore
+        var html = [
+          '<style id="'+ tagId +'">',
+          '  .default-bay-window { position: fixed; }',
+          '  .default-bay-window__img-wrap { display: block; }',
+          '  .default-bay-window__img { display: block; height: 100%; width: 100%; object-fit: cover; }',
+          // reset
+          '  .default-bay-window__close { -webkit-box-shadow: none; box-shadow: none; outline: none; cursor: pointer; }',
+          '  .default-bay-window__close { display: block; margin-left: auto; background: #fff; color: #333; border: 1px solid #999; }',
+          '  .default-bay-window__close:hover { background: #eee; }',
+          '</style>'
+        ].join('')
+        $('head').append(html)
+      }
+      appendStyleTag()
+
       var settings = {
-        id: "jsBayWindow", // 元素ID
-        height: "130px", // 高度
-        width: "230px", // 宽度
-        src: "", // 图片src
-        href: "", // 跳转链接
-      };
-      $.extend(settings, options || {});
+        id: 'jsBayWindow', // 元素ID
+        height: '130px', // 高度
+        width: '230px', // 宽度
+        src: '', // 图片src
+        href: '', // 跳转链接
+        target: '__blank',
+        closeText: '关闭',
+      }
+      $.extend(settings, options || {})
+      if ($('#' + settings.id).length) return
       // prettier-ignore
-      var imageWrapStyle = "display: block; height: " + settings.height + "; width: " + settings.width + ";";
+      var imageWrapStyle = "height: " + settings.height + "; width: " + settings.width + ";";
       // prettier-ignore
       var html = [
-        '<div id="' + settings.id + '" style="position: fixed; ">',
-        '  <a href="' + settings.href + '" style="' + imageWrapStyle + '">',
-        '    <img src="' + settings.src +'" style="display: block; height: 100%; width: 100%" />',
+        '<div id="' + settings.id + '" class="default-bay-window">',
+        '  <a class="default-bay-window__img-wrap" href="' + settings.href + '" style="' + imageWrapStyle + '" target="'+ settings.target +'">',
+        '    <img class="default-bay-window__img" src="' + settings.src +'" />',
         '  </a>',
-        '  <a class="jsBayWindowClose" href="javascript:;" style="display: block; width: 36px; text-align: center; margin-left: auto; font-size: 12px;color: #000;background-color: #fff;line-height: 20px;">关闭</a>',
+        '  <button class="jsBayWindowClose default-bay-window__close" type="button">'+ settings.closeText +'</button>',
         '</div>',
       ].join("");
 
-      var $bayWindow = $(html);
-      $("body").append($bayWindow);
-      $bayWindow.find(".jsBayWindowClose").on("click", function () {
-        var bayWindow = $bayWindow.data(BAY_WINDOW_DATA_KEY);
+      var $bayWindow = $(html)
+      $('body').append($bayWindow)
+      $bayWindow.find('.jsBayWindowClose').on('click', function () {
+        var bayWindow = $bayWindow.data(BAY_WINDOW_DATA_KEY)
         if (bayWindow) {
-          bayWindow.destroy();
+          $(this).off('click')
+          bayWindow.destroy()
         }
-      });
-      return $bayWindow;
+      })
+      return $bayWindow
     },
-  });
+  })
 
   /**
    * 飘窗类
@@ -102,103 +106,103 @@
    * @param {*} options 同BayWindow.DEFAULTS
    */
   var BayWindow = function (element, options) {
-    this.$element = $(element);
-    this.options = $.extend({}, BayWindow.DEFAULTS, options);
-    this.isToLeft = false; // 是否向左运动
-    this.isToTop = false; // 是否向上运动
-    // this.stepLength = 1; // 每次移动距离
-    this.timeoutId; // setTimout返回值
-    this.top = this.options.startTop; // css top
-    this.left = this.options.startLeft; // css left
-    this.lastTime = 0;
+    this.$element = $(element)
+    this.options = $.extend({}, BayWindow.DEFAULTS, options)
+    this.isToLeft = false // 是否向左运动
+    this.isToTop = false // 是否向上运动
+    this.cancelId // requestAnimationFrame
+    this.top = this.options.startTop // css top
+    this.left = this.options.startLeft // css left
+    this.lastTime = 0
 
     this.$element.css({
-      position: "fixed",
+      position: 'fixed',
       top: SUPPORT_TRANSFORM ? 0 : this.top,
       left: SUPPORT_TRANSFORM ? 0 : this.left,
-      cursor: "pointer",
+      cursor: 'pointer',
       transform: SUPPORT_TRANSFORM
-        ? "translate(" + this.left + "px," + this.top + "px)"
-        : "",
+        ? 'translate(' + this.left + 'px,' + this.top + 'px)'
+        : '',
       zIndex: this.options.zIndex,
-    });
+    })
 
-    this.$element.hover(this.stop.bind(this), this.run.bind(this));
-  };
+    this.$element.hover(this.stop.bind(this), this.run.bind(this))
+  }
 
   BayWindow.DEFAULTS = {
-    // speed: 16.7, // 运动间隔时
     stepLength: 1, // 每16.7ms移动的距离
-    startTop: 10000, // x轴起始坐标， 默认从底部向上运动
+    startTop: 0, // x轴起始坐标， 默认从底部向上运动
     startLeft: 0, // y轴起始坐标
-    zIndex: 10, // css z-index
-  };
+    zIndex: 9999, // css z-index
+  }
 
   BayWindow.prototype._nextStep = function (stepLength) {
-    stepLength = stepLength || 1;
-    var winWidth = $(window).width() - this.$element.width() - 10;
-    var winHeight = $(window).height() - this.$element.height() - 10;
+    stepLength = stepLength || 1
+    var winWidth = $(window).width() - this.$element.width() - 10
+    var winHeight = $(window).height() - this.$element.height() - 10
 
     // 向左运动，要减小left值
-    this.left += (this.isToLeft ? -1 : 1) * stepLength;
+    this.left += (this.isToLeft ? -1 : 1) * stepLength
     if (this.left >= winWidth) {
-      this.left = winWidth;
-      this.isToLeft = true;
+      this.left = winWidth
+      this.isToLeft = true
     } else if (this.left <= 0) {
-      this.left = 0;
-      this.isToLeft = false;
+      this.left = 0
+      this.isToLeft = false
     }
 
     // 向上运动，要减小top值
-    this.top += (this.isToTop ? -1 : 1) * stepLength;
+    this.top += (this.isToTop ? -1 : 1) * stepLength
     if (this.top >= winHeight) {
-      this.top = winHeight;
-      this.isToTop = true;
+      this.top = winHeight
+      this.isToTop = true
     } else if (this.top <= 0) {
-      this.top = 0;
-      this.isToTop = false;
+      this.top = 0
+      this.isToTop = false
     }
-    
+
     if (SUPPORT_TRANSFORM) {
       this.$element.css({
-        transform: "translate(" + this.left + "px," + this.top + "px)",
-      });
+        transform: 'translate(' + this.left + 'px,' + this.top + 'px)',
+      })
     } else {
       this.$element.css({
         left: this.left,
         top: this.top,
-      });
+      })
     }
-  };
+  }
 
   BayWindow.prototype.run = function (e) {
-    var stepLength = this.options.stepLength;
-    var timeSpace = e - this.lastTime;
-    this.lastTime = e;
+    this.stop()
+    var stepLength = this.options.stepLength
+    var timeSpace = e - this.lastTime
+    this.lastTime = e
 
     // 小于16，说明是高刷屏，按倍数计算
     // 大于16，即使大很多，也按16.7计算
     if (timeSpace && timeSpace < 16) {
-      stepLength = ((timeSpace / 16.7) * stepLength).toFixed(2);
+      stepLength = ((timeSpace / 16.7) * stepLength).toFixed(2)
     }
-    this._nextStep(stepLength);
-    this.timeoutId = requestAnimationFrame(this.run.bind(this));
-  };
+    this._nextStep(stepLength)
+    this.cancelId = requestAnimationFrame(this.run.bind(this))
+  }
 
   /**
    * 停止运动
    */
   BayWindow.prototype.stop = function () {
-    cancelAnimationFrame(this.timeoutId);
-  };
+    cancelAnimationFrame(this.cancelId)
+  }
 
   /**
+   * 销毁
    * 停止运动，删除元素
    */
   BayWindow.prototype.destroy = function () {
-    this.stop();
-    this.$element.remove();
-  };
+    this.stop()
+    this.$element.remove()
+  }
 
   /**
    *
@@ -207,18 +211,66 @@
    */
   $.fn.bayWindow = function (option) {
     if (!option) {
-      option = {};
+      option = {}
+    }
+    if (!this || !this.length) {
+      console.warn('Bay Window no matching elements')
     }
     return this.each(function () {
-      var $this = $(this);
-      var data = $this.data(BAY_WINDOW_DATA_KEY);
-      var options = typeof option === "object" ? option : {};
-      var method = typeof option === "string" ? option : "run";
+      var $this = $(this)
+      var data = $this.data(BAY_WINDOW_DATA_KEY)
+      var options = typeof option === 'object' ? option : {}
+      var method = typeof option === 'string' ? option : 'run'
 
-      if (data) data.stop();
-      data = new BayWindow(this, options);
-      $this.data(BAY_WINDOW_DATA_KEY, data);
-      data[method]();
-    });
-  };
-});
+      if (!data || typeof option === 'object') {
+        if (data) data.stop()
+        data = new BayWindow(this, options)
+        $this.data(BAY_WINDOW_DATA_KEY, data)
+      }
+
+      if (!data[method]) {
+        console.warn('"' + method + '"', 'is not bay window method.')
+      } else {
+        data[method]()
+      }
+    })
+  }
+
+  /**
+   * support ie < 9
+   * requestAnimationFrame polyfill
+   *
+   */
+  function requestAnimationFramePolyfill() {
+    // ie <= 8
+    if (!Date.now)
+      Date.now = function () {
+        return new Date().getTime()
+      }
+
+    // ie <= 9
+    var vendors = ['webkit', 'moz']
+    for (var i = 0; i < vendors.length && !window.requestAnimationFrame; ++i) {
+      var vp = vendors[i]
+      window.requestAnimationFrame = window[vp + 'RequestAnimationFrame']
+      window.cancelAnimationFrame =
+        window[vp + 'CancelAnimationFrame'] ||
+        window[vp + 'CancelRequestAnimationFrame']
+    }
+    if (
+      /iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent) || // iOS6 is buggy
+      !window.requestAnimationFrame ||
+      !window.cancelAnimationFrame
+    ) {
+      var lastTime = 0
+      window.requestAnimationFrame = function (callback) {
+        var now = Date.now()
+        var nextTime = Math.max(lastTime + 16, now)
+        return setTimeout(function () {
+          callback((lastTime = nextTime))
+        }, nextTime - now)
+      }
+      window.cancelAnimationFrame = clearTimeout
+    }
+  }
+})
